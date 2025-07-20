8a2a96b210f58ea04ffefc8b7cc3d5d383b7daa2
MamoudDahy11 <dahym2028@gmail.com>
2025-07-20 16:31:19 +0300

    edit card and create custom widget for this card and use listTile in card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widget/custom_search_icon.dart';
+import 'package:note_app/widget/note_view_body.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
@@ -14,62 +15,7 @@ class NoteView extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontSize: 30),
         ),
       ),
-      body: Container(
-        margin: EdgeInsets.all(16),
-        padding: EdgeInsets.all(16),
-        height: 230,
-        decoration: BoxDecoration(
-          color: Colors.amber.shade100,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, top: 16),
-              child: Text(
-                'Flutter tips',
-                style: TextStyle(color: Colors.black, fontSize: 35),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.delete, size: 40, color: Colors.black45),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Build My career with',
-                style: TextStyle(color: Colors.black54, fontSize: 18),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Mahmoud Dahy',
-                style: TextStyle(color: Colors.black54, fontSize: 18),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Text(
-                    'May 21/2022',
-                    style: TextStyle(color: Colors.black45),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: NoteViewBody(),
     );
   }
 }
---
+++ lib/widget/note_view_body.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+
+class NoteViewBody extends StatelessWidget {
+  const NoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 230,
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.only(left: 24, top: 24, bottom: 24),
+      decoration: BoxDecoration(
+        color: Color(0XFFFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(color: Colors.black, fontSize: 35),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 40, color: Colors.black),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Text(
+                'Build My career with Mahmoud Dahy',
+                style: TextStyle(color: Colors.black54, fontSize: 19),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text('May 21/2022', style: TextStyle(color: Colors.black45)),
+          ),
+        ],
+      ),
+    );
+  }
+}
