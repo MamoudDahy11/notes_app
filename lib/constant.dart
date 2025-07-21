28014be34de10e52fcccb09e843abd523fc9b54d
MamoudDahy11 <dahym2028@gmail.com>
2025-07-21 20:25:16 +0300

    init hive and open box

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
 const kPrimaryColor = Color(0XFF71FFE6);
+
+const kNotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constant.dart';
 import 'package:note_app/screens/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   return runApp(NotesApp());
 }
 
