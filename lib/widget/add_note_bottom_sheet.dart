af6ea06bb5cfae843b3138b901e42e1e2b8bb24c
MamoudDahy11 <dahym2028@gmail.com>
2025-07-22 16:23:07 +0300

    add note add add note cubit to add note bottom sheet

--- lib/widget/add_note_bottom_sheet.dart
+++ lib/widget/add_note_bottom_sheet.dart
@@ -1,61 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/widget/custom_bottom.dart';
-import 'package:note_app/widget/custom_text_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubit/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/widget/add_note_form.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(child: AddNoteForm());
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({super.key});
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, subTitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          const SizedBox(height: 15),
-          CustomTextField(
-            text: 'Title',
-            onSaved: (value) {
-              title = value;
-            },
-          ),
-          CustomTextField(
-            text: 'Content',
-            maxLine: 5,
-            onSaved: (value) {
-              subTitle = value;
-            },
-          ),
-          const SizedBox(height: 80),
-          CustomBotton(
-            text: 'Add',
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-              }
-            },
-          ),
-          const SizedBox(height: 50),
-        ],
+    return SingleChildScrollView(
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteFailure) {
+            print('failed ${state.errorMessage}');
+          }
+          if (state is AddNoteSuccess) {
+            Navigator.of(context).pop();
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoading ? true : false,
+            child: const AddNoteForm(),
+          );
+        },
       ),
     );
   }
---
+++ lib/widget/add_note_form.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+import 'package:note_app/widget/custom_bottom.dart';
+import 'package:note_app/widget/custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(height: 15),
+          CustomTextField(
+            text: 'Title',
+            onSaved: (value) {
+              title = value;
+            },
+          ),
+          CustomTextField(
+            text: 'Content',
+            maxLine: 5,
+            onSaved: (value) {
+              subTitle = value;
+            },
+          ),
+          const SizedBox(height: 80),
+          CustomBotton(
+            text: 'Add',
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+              }
+            },
+          ),
+          const SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+}
