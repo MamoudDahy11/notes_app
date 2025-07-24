84155b1120fccb6075c803a92302c9de4d0b13e7
Mahmoud Dahy <dahym2028@gmail.com>
2025-07-24 19:34:43 +0300

    finish app

--- lib/cubit/add_note_cubit/add_note_cubit.dart
+++ lib/cubit/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:note_app/constant.dart';
@@ -8,7 +9,9 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = const Color(0XFF0B2027);
   Future<void> addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var noteBox = Hive.box<NoteModel>(kNotesBox);
--- lib/widget/colors_list_view.dart
+++ lib/widget/colors_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubit/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/cubit/notes_cubit/notes_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
@@ -50,6 +53,7 @@ class _ListViewItemState extends State<ListViewItem> {
             child: GestureDetector(
               onTap: () {
                 currentIndex = index;
+                BlocProvider.of<AddNoteCubit>(context).color = colors[index];
                 setState(() {});
               },
               child: ColorItem(
